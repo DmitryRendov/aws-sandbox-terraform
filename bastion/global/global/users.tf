91a6bec20a331d44303cf0ac51c181827ba36ac0
Dmitry Rendov <dmitry_rendov@epam.com>
2021-10-11 15:51:03 +0300

    04 - Onboard Production environment

--- bastion/global/global/users.tf
+++ bastion/global/global/users.tf
@@ -2,11 +2,13 @@ module "dmitry_rendov" {
   source = "../../../modules/user-roles/v1"
   name   = "dmitry_rendov"
 
-  audit_policy_arns   = ["arn:aws:iam::aws:policy/AdministratorAccess"]
-  bastion_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  audit_policy_arns      = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  bastion_policy_arns    = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  production_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
 
   providers = {
-    aws.audit = aws.audit
+    aws.audit      = aws.audit
+    aws.production = aws.production
   }
 }
 
@@ -14,11 +16,13 @@ module "aliaksei_kliashchonak" {
   source = "../../../modules/user-roles/v1"
   name   = "aliaksei_kliashchonak"
 
-  audit_policy_arns   = ["arn:aws:iam::aws:policy/PowerUserAccess"]
-  bastion_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
+  audit_policy_arns      = ["arn:aws:iam::aws:policy/PowerUserAccess"]
+  bastion_policy_arns    = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
+  production_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
 
   providers = {
-    aws.audit = aws.audit
+    aws.audit      = aws.audit
+    aws.production = aws.production
   }
 }
 
@@ -26,10 +30,12 @@ module "mikhail_parkun" {
   source = "../../../modules/user-roles/v1"
   name   = "mikhail_parkun"
 
-  audit_policy_arns   = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
-  bastion_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
+  audit_policy_arns      = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
+  bastion_policy_arns    = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
+  production_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
 
   providers = {
-    aws.audit = aws.audit
+    aws.audit      = aws.audit
+    aws.production = aws.production
   }
 }
