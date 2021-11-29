a1b846b80e038f51fb482e55b9f8e5b1d85f3647
Dmitry Rendov <dmitry_rendov@epam.com>
2021-11-29 08:43:55 +0300

    86 - Introduce API GW for the Serverless website

--- production/roles/serverless/locals.tf
+++ production/roles/serverless/locals.tf
@@ -1,4 +1,16 @@
 locals {
   team      = "developers"
   role_name = "serverless"
+
+  # API Gateway
+  api_gateway_name           = "${module.label.id}-public"
+  api_version                = "v1"
+  api_type                   = "EDGE"
+  api_metrics_enabled        = "false"
+  api_data_trace_enabled     = "false"
+  api_logging_level          = "INFO"
+  api_throttling_rate_limit  = 1000
+  api_throttling_burst_limit = 100
+  api_logs_retention_in_days = 30
+  api_xray_tracing_enabled   = "false"
 }
