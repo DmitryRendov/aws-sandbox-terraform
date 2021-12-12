afb102a422cda2f22c87bc34aa612a8223e3eca5
Dmitry Rendov <dmitry_rendov@epam.com>
2021-12-12 12:12:06 +0300

    60 - Moved to global folder, updated the output

--- production/global/route53/locals.tf
+++ production/global/route53/locals.tf
@@ -1,6 +1,6 @@
 locals {
   team      = "ops"
-  role_name = "global-infra"
+  role_name = "route53"
 
   domain_name = "xn--b1add1bfm.xn--90ais" // "девопc.бел"
 }
--- production/global/route53/outputs.tf
+++ production/global/route53/outputs.tf
@@ -5,10 +5,10 @@ output "name_servers" {
 
 output "zone_id" {
   description = "Zone ID of the Route53 zone"
-  value       = module.hosted_zones.route53_zone_zone_id
+  value       = join("", values(module.hosted_zones.route53_zone_zone_id))
 }
 
 output "zone_name" {
   description = "Name of the Route53 zone"
-  value       = module.hosted_zones.route53_zone_name
+  value       = join("", values(module.hosted_zones.route53_zone_name))
 }
