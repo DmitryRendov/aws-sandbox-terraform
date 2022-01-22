d44e38e069a1209fb5ba3d59dce9c5527f98e915
Ilya Melnik <56299215+obstaclex@users.noreply.github.com>
2022-01-22 11:46:27 +0300

    57/scp security_protect added (#103)
    * scp security_protect added
    
    * 57 - Add super-user to exception
    
    Co-authored-by: Ilya Melnik <ilya.melnik.svc@gmail.com>
    Co-authored-by: Dmitry Rendov <dmitry_rendov@epam.com>

---
+++ bastion/global/accounts/scp_protect_security_settings.tf
@@ -0,0 +1,40 @@
+data "aws_iam_policy_document" "security_settings" {
+  statement {
+    sid = "DenyDeleteAccessAnalyzer"
+    actions = [
+      "access-analyzer:DeleteAnalyzer"
+    ]
+    resources = ["*"]
+    effect    = "Deny"
+  }
+
+  statement {
+    sid = "DenyCreateEBSWithoutEncryption"
+    actions = [
+      "ec2:CreateVolume"
+    ]
+    resources = ["*"]
+    effect    = "Deny"
+    condition {
+      test     = "Bool"
+      variable = "ec2:Encrypted"
+      values   = ["false"]
+    }
+  }
+
+  statement {
+    sid = "PreventModifyingS3PublicAccessBlock"
+    actions = [
+      "s3:PutBucketPublicAccessBlock"
+    ]
+    resources = ["*"]
+    effect    = "Deny"
+    condition {
+      test     = "StringNotLike"
+      variable = "aws:PrincipalARN"
+      values = [
+        "arn:aws:iam::*:role/super-user"
+      ]
+    }
+  }
+}
