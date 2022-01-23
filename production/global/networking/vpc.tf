b25bf76cc33f9a43dea0091fa8e34dc6a08901f9
Dmitry Rendov <dmitry_rendov@epam.com>
2022-01-23 12:34:29 +0300

    59 - disable NAT for now

--- production/global/networking/vpc.tf
+++ production/global/networking/vpc.tf
@@ -12,7 +12,7 @@ module "subnets" {
   azs                = ["us-east-1a", "us-east-1b"]
   public_subnets     = ["10.100.0.0/20", "10.100.16.0/20"]
   private_subnets    = ["10.100.32.0/20", "10.100.48.0/20"]
-  enable_nat_gateway = true
+  enable_nat_gateway = false
   single_nat_gateway = true
 
   label = module.label
