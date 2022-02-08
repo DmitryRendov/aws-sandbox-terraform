29f65c4c4344d0ab5027a2066a2a97ba197ff3df
Dmitry Rendov <DmitryRendov@users.noreply.github.com>
2022-02-08 11:24:50 +0300

    117 - Fix Ops access to TF state bucket (#119)

--- bastion/global/global/locals.tf
+++ bastion/global/global/locals.tf
@@ -9,12 +9,17 @@ locals {
 
 
   developer_policies = {
-    developer_prod_policy_arns = [
+    developer_production_policy_arns = [
       "arn:aws:iam::aws:policy/ReadOnlyAccess",
       aws_iam_policy.developer_permissions.arn,
       data.terraform_remote_state.production_serverless.outputs.serverless_policy_arn,
     ]
   }
 
-
+  ops_policies = {
+    ops_bastion_policy_arns = [
+      "arn:aws:iam::aws:policy/ReadOnlyAccess",
+      aws_iam_policy.ops_bastion_role_iam.arn,
+    ]
+  }
 }
