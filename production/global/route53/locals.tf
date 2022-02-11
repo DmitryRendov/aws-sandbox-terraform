603a6b091f4d4fc5451b08d36309b7b804906493
Ilya Melnik <ilya.melnik.svc@gmail.com>
2022-02-11 23:58:18 +0300

    create certificate

--- production/global/route53/locals.tf
+++ production/global/route53/locals.tf
@@ -1,6 +1,5 @@
 locals {
-  team      = "ops"
-  role_name = "route53"
-
+  team        = "ops"
+  role_name   = "route53"
   domain_name = "cloudology.by"
 }
--- production/global/route53/route53.tf
+++ production/global/route53/route53.tf
@@ -7,3 +7,11 @@ module "hosted_zones" {
   ]
   label = module.label
 }
+
+module "acm_request_certificate" {
+  source                    = "../../../modules/base/acm/v1"
+  domain_name               = local.domain_name
+  subject_alternative_names = ["*.${local.domain_name}"]
+  zone_name                 = module.hosted_zones.route53_zone_name[local.domain_name]
+  label                     = module.label
+}
