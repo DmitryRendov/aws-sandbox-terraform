36ef1fbafc52251bfe1244f0c587db459e7945e2
Ilya Melnik <56299215+obstaclex@users.noreply.github.com>
2022-02-13 10:09:32 +0300

    added atlantis role (#122)
    Co-authored-by: Ilya Melnik <ilya.melnik.svc@gmail.com>

---
+++ production/roles/atlantis/remote-state.tf
@@ -0,0 +1,25 @@
+data "terraform_remote_state" "global" {
+  backend = "s3"
+
+  config = {
+    key          = "${var.account_name}/global/global"
+    bucket       = var.terraform_remote_state_bucket
+    region       = var.terraform_remote_state_region
+    profile      = "sts"
+    role_arn     = "arn:aws:iam::${var.aws_account_map["bastion"]}:role/${var.terraform_exec_role}"
+    session_name = "terraform"
+  }
+}
+
+data "terraform_remote_state" "networking" {
+  backend = "s3"
+
+  config = {
+    key          = "${var.account_name}/global/networking"
+    bucket       = var.terraform_remote_state_bucket
+    region       = var.terraform_remote_state_region
+    profile      = "sts"
+    role_arn     = "arn:aws:iam::${var.aws_account_map["bastion"]}:role/${var.terraform_exec_role}"
+    session_name = "terraform"
+  }
+}
